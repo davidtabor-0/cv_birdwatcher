26dd3e36ed27ea62166c85ab6525cafa86daf26b
David Tabor <1davidtabor1@gmail.com>
2025-05-23 12:33:50 -0700

    Added rolling averaging to improve stability

--- reciever.py
+++ reciever.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 import time
+from collections import deque
 
 CONF_THRESH = 0.7 # Adjust this to change the confidence level.
 DESIRED_CLASS = 14 # Bird. 
@@ -14,7 +15,7 @@ model = YOLO('yolov8n.pt')
 
 def process_stream():
     cap = cv2.VideoCapture(STREAM_URL)
-    
+    bird_count_queue = deque()
     if not cap.isOpened():
         print(f"Error: Could not open stream at {STREAM_URL}")
         print("Make sure your Raspberry Pi is running")
@@ -26,6 +27,7 @@ def process_stream():
         while True:
             time.sleep(0.25) # Reduce frame rate.
             ret, frame = cap.read()
+            bird_count = 0
             
             if not ret:
                 print("Failed to read frame")
@@ -38,17 +40,22 @@ def process_stream():
                     for box in boxes:
                         confidence = box.conf[0].cpu().numpy()
                         class_id = int(box.cls[0].cpu().numpy())
+
                         if confidence > CONF_THRESH:
                             if class_id == DESIRED_CLASS:
+                                bird_count += 1
                                 x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
                                 class_name = model.names[class_id]
                                 
                                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
                                 cv2.putText(frame, f'Class {class_name} ({class_id}): {confidence:.2f}', 
                                         (int(x1), int(y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            
+                                
+            bird_count_queue.append(bird_count)
+            if len(bird_count_queue) > 5:
+                bird_count_queue.popleft()
+            print(f"Number of birds: {np.ceil(np.mean(bird_count_queue))}")
             cv2.imshow('YOLO Detection', frame)
-            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
                 
